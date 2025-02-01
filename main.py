3e22996c52af8e394583559c0a6e6ce156806108
Michael <me@chichunov.ru>
2025-02-01 13:36:14 +0300

    add docker-compose

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import logging
 from telegram import Update
 from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+from pathlib import Path
 
 
 logging.basicConfig(
@@ -17,26 +18,34 @@ assert TELEGRAM_TOKEN, "Token not found in env"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    sender_name = update.message.from_user.first_name  # type: ignore
+    sender_name = update.message.from_user.first_name
     await context.bot.send_message(
-            chat_id=update.effective_chat.id,  # type: ignore
+            chat_id=update.effective_chat.id,
             text=f"Привет, {sender_name}!",
         )
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,  # type: ignore
-        text=update.message.text,  # type: ignore
-    )
+async def document(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    document = update.message.document
+    file_name = document.file_name
+    file = await document.get_file()
+    await file.download_to_drive(custom_path=Path(file_name))
+
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🤷‍♂️")
 
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
     start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    document_handler = MessageHandler(filters.Document.FileExtension("doc"), document)
+    unknown_command_handler = MessageHandler(filters.COMMAND, unknown)
 
     application.add_handler(start_handler)
-    application.add_handler(echo_handler)
+    application.add_handler(document_handler)
+
+    application.add_handler(unknown_command_handler)
 
     application.run_polling()
